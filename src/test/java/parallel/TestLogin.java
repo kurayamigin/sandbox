c051e115c33720098dea7a38dc3b2c0533252a0f
kurayami gin <knd_julian@hotmail.com>
2025-06-13 13:08:46 -0600

    testing ip proxy

--- src/test/java/parallel/TestLogin.java
+++ src/test/java/parallel/TestLogin.java
@@ -7,9 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
